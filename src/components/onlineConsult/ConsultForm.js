3fe21475de3e796c4fcdd86b88f9e8ddb49bd4bb
alex <alexcarvajal2404@gmail.com>
2022-03-08 23:07:17 -0500

    vista de consulta online step 1

---
+++ src/components/onlineConsult/ConsultForm.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+import {Box, Button, Paper, TextField, Typography} from "@mui/material";
+
+export default function ConsultForm() {
+  const [state, setState] = useState(false);
+
+  useEffect(() => {
+    // Effect code here
+    return () => {
+      //your cleanup code codes here
+    };
+  }, [state])
+
+  return (
+   <Box sx={{my: {xs: 15, md: 0}, display: {md: 'grid'}, placeItems: {md: 'center'}, height: {md: '80vh'}}}>
+     <Paper elevation={3} sx={{p: 3}}>
+       <Typography>Email</Typography>
+       <TextField
+          fullWidth
+          placeholder='ejemplo@123.com'
+        />
+        <Button sx={{ mt: 2 }} variant='contained' fullWidth>Siguiente</Button>
+      </Paper>
+   </Box>
+  )
+}
+ 
